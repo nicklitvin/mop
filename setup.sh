a2ea6b31123f01410b092183ca25a9169844de3c
nicklitvin <nick.litvinov9@gmail.com>
2025-04-01 23:01:29 -0700

    updated sh

--- setup.sh
+++ setup.sh
@@ -6,17 +6,19 @@ cd web && npm install && cd ../server && npm install
 
 # Configure environment variables
 echo "Setting up environment variables..."
+touch server/.env
 echo "DATABASE_URL=file:./dev.db?socket_timeout=10" > server/.env
 echo "LISTEN_PORT=3000" >> server/.env
 echo "GPT_API_KEY=<your_openai_api_key>" >> server/.env
 echo "WEB_IP=http://localhost" >> server/.env
 
+touch web/.env
 echo "VITE_API_URL=\"http://localhost:80/api\"" > web/.env
 
 # Run database migrations
 echo "Running database migrations..."
 cd server && npm run db
-
+  
 # Build the web application
 echo "Building the web application..."
 cd ../web && npm run build
